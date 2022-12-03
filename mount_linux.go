beacc4e5c4a21e8cab163bf2aafa995647233925
Caleb Gardner <calebq23@gmail.com>
2022-12-03 01:56:55 -0600

    Replace fusermount to fusermount3

--- mount_linux.go
+++ mount_linux.go
@@ -84,13 +84,13 @@ func mount(dir string, conf *mountConfig, ready chan<- struct{}, errp *error) (f
 	// if os.Getuid == 0, try mountdev. That could fail for all sorts of reasons,
 	// and if it does, just keep on this other path.
 	/*
-	if os.Getuid() == 0 {
-		f, err := mountdev(dir, conf)
-		if err == nil {
-			return f, err
+		if os.Getuid() == 0 {
+			f, err := mountdev(dir, conf)
+			if err == nil {
+				return f, err
+			}
+			log.Printf("mount: mountdev failed with %v, trying fusermount", err)
 		}
-		log.Printf("mount: mountdev failed with %v, trying fusermount", err)
-	}
 	*/
 
 	fds, err := syscall.Socketpair(syscall.AF_FILE, syscall.SOCK_STREAM, 0)
@@ -105,7 +105,7 @@ func mount(dir string, conf *mountConfig, ready chan<- struct{}, errp *error) (f
 	defer readFile.Close()
 
 	cmd := exec.Command(
-		"fusermount",
+		"fusermount3",
 		"-o", conf.getOptions(),
 		"--",
 		dir,
